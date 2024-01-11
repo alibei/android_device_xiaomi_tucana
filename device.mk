a8241deba7913c95cba84a1fe451d7325a47b055
alibei <sebastiankotzias83@gmail.com>
2024-01-11 12:08:26 +0100

    tucana: Build vendor.qti.hardware.display.mapper@3.0.vendor
    Fixes "dlopen failed: library "vendor.qti.hardware.display.mapper@3.0.so" not found: needed by /vendor/lib64/sensors.ssc.so in namespace (default)"
    
    Signed-off-by: alibei <sebastiankotzias83@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_f4_41_06_0a_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_f4_41_06_0a_fhd_cmd_dsi_panel.xml \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_f4_42_06_0c_fhd_cmd_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_f4_42_06_0c_fhd_cmd_dsi_panel.xml
 
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.display.mapper@3.0.vendor
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     libudfpshandler \
